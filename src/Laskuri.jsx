f6a65004e3a4a7ff0180368658a4fd56c70e8a6f
Simo Siren <sirensimo7@gmail.com>
2022-01-19 23:04:46 +0200

    Kotitehtävien ratkaisumallit

--- src/Laskuri.jsx
+++ src/Laskuri.jsx
@@ -13,6 +13,10 @@ const [luku, setLuku] = useState(0)
 
         <button onClick={() => setLuku(luku + 1)}>+</button>
 
+        <button onClick={() => setLuku(luku - 1)}>-</button>
+
+        <button onClick={() => setLuku(0)}>Reset</button>
+
 
         <button onClick={huomio}>huomio</button>
 
