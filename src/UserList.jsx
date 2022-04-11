09cc8a2d6fe8feeed5156772b5d2f183d0778332
Simo Siren <sirensimo7@gmail.com>
2022-04-11 16:22:27 +0300

    user lisäys ja md5 salaus

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import React, {useState, useEffect} from 'react'
 import UserService from './services/User'
+import UserAdd from './UserAdd'
 
 const UserList = ({setMessage, setIsPositive, setShowMessage}) => {
 
@@ -17,12 +18,12 @@ useEffect(() => {
   UserService.getAll()
   .then(data => {
     setUsers(data)
-})
-},[lisäystila, reload, muokkaustila] // Nämä statet jos muuttuu niin useEffect() ajetaan uudestaan
-)
+        })
+    },[lisäystila, reload, muokkaustila] // Nämä statet jos muuttuu niin useEffect() ajetaan uudestaan
+  )
 
   //Hakukentän onChange tapahtumankäsittelijä
-  const handleSearchInputChange = (event) => {
+const handleSearchInputChange = (event) => {
     setSearch(event.target.value.toLowerCase())
 }
 
@@ -32,51 +33,55 @@ const editUsers = (user) => {
 }
 
   return (
-    <>
-        <h1><nobr>Users</nobr>
+        <>
+            <h1><nobr>Users</nobr>
 
-                {!lisäystila && <button className="nappi" onClick={() => setLisäystila(true)}>Add new</button>}</h1>
+            {lisäystila && <UserAdd setLisäystila={setLisäystila} 
+            setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage} />}
 
-                {!lisäystila && !muokkaustila &&
-                <input placeholder="Search by Last Name" value={search} onChange={handleSearchInputChange} />
+            {!lisäystila && <button className="nappi" onClick={() => setLisäystila(true)}>Add new</button>}</h1>
+
+            {!lisäystila && !muokkaustila &&
+            <input placeholder="Search by Last Name" value={search} onChange={handleSearchInputChange} />
+            }
+
+            {!lisäystila && !muokkaustila &&
+            <table id="userTable">
+                <thead>
+                    <tr>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Email</th>
+                        <th>Accesslevel</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+        
+                {users && users.map(u =>
+                {
+                    const lowerCaseName = u.lastname.toLowerCase()
+                    if (lowerCaseName.indexOf(search) > -1) {
+                        return(
+                            <tr key={u.userId}>
+                                <td>{u.firstname}</td>
+                                <td>{u.lastname}</td>
+                                <td>{u.email}</td>
+                                <td>{u.accesslevelId}</td>
+                            </tr>
+                            
+                                )
+                            }
+                        }
+                    )
                 }
 
-                <table id="userTable">
-                    <thead>
-                        <tr>
-                            <th>Firstname</th>
-                            <th>Lastname</th>
-                            <th>Email</th>
-                            <th>Accesslevel</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                </tbody>
 
-            
-                    {users && users.map(u =>
-                    {
-                        const lowerCaseName = u.lastname.toLowerCase()
-                        if (lowerCaseName.indexOf(search) > -1) {
-                            return(
-                                <tr key={u.userId}>
-                                    <td>{u.firstname}</td>
-                                    <td>{u.lastname}</td>
-                                    <td>{u.email}</td>
-                                    <td>{u.accesslevelId}</td>
-                                </tr>
-                                
-                            )
-                                    }
-                                }
-                            )
-                        }
-
-                        </tbody>
-
-                        </table>
-
-                    </>
+            </table>
+            }
+         </>
         )
-        }
+    }
 
 export default UserList
