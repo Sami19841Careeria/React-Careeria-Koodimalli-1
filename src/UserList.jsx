0672eb0fa6e5918e4762903e3db8d4d472d2925e
Sami Palomäki <96203498+Sami19841Careeria@users.noreply.github.com>
2022-12-08 15:07:24 +0200

    päivitys user editiin1.9

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -30,49 +30,108 @@ const handleSearchInputChange = (event) => {
     setSearch(event.target.value.toLowerCase())
 }
 
-const editUser = (user) => {
+const editUsers = (user) => {
   setMuokattavaUser(user)
   setMuokkaustila(true)
 }
 
-return (
-    <>
-        <h1><nobr style={{ cursor: 'pointer' }}
+  return (
+        <>
+           <h1><nobr style={{ cursor: 'pointer' }}
                 onClick={() => setShowUsers(!showUsers)}>Users</nobr>
 
                 {!lisäystila && <button className="nappi" onClick={() => setLisäystila(true)}>Add new</button>}</h1>
+            {lisäystila && <UserAdd setLisäystila={setLisäystila} 
+            setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage} />}
 
-                {!lisäystila && !muokkaustila &&
-                <input placeholder="Search by Last name" value={search} onChange={handleSearchInputChange} />
-                }
-
-                {lisäystila && <UserAdd setLisäystila={setLisäystila} 
-                setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage}
-                />}
-
-                {muokkaustila && <UserEdit setMuokkaustila={setMuokkaustila} 
+            
+            {muokkaustila && <UserEdit setMuokkaustila={setMuokkaustila} 
                 setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage}
                 muokattavaUser={muokattavaUser}
                 />}
 
-        {
-            !lisäystila && !muokkaustila && showUsers && users && users.map(u =>
-              {
-                const lowerCaseName = u.userName.toLowerCase()
-                if (lowerCaseName.indexOf(search) > -1) {
-                    return(
-                <User key={u.userId} user={u} reloadNow={reloadNow} reload={reload}
-                setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage}
-                editUser={editUser}
-                />
-              )
-                    }
-                  }
-            )
-        }
+            {!lisäystila && !muokkaustila &&
+            <input placeholder="Search by Last Name" value={search} onChange={handleSearchInputChange} />
+            }
 
-    </>
-  )
+            {!lisäystila && !muokkaustila &&
+            <table id="userTable">
+                <thead>
+                    <tr>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Email</th>
+                        <th>Accesslevel</th>
+                        <button>Delete</button>
+                        <button>Edit</button>
+                    </tr>
+                </thead>
+                <tbody>
+
+        
+                {showUsers && users && users.map(u =>
+                {
+                    const lowerCaseName = u.lastname.toLowerCase()
+                    if (lowerCaseName.indexOf(search) > -1) {
+                        return(
+                            <tr key={u.userId}>
+                                <td>{u.firstname}</td>
+                                <td>{u.lastname}</td>
+                                <td>{u.email}</td>
+                                <td>{u.accesslevelId}</td>
+                            </tr>
+                            
+                                )
+                            }
+                        }
+                    )
+                }
+
+                </tbody>
+
+            </table>
+            }
+         </>
+        )
     }
+
+    // return (
+    //     <>
+    //         <h1><nobr style={{ cursor: 'pointer' }}
+    //                 onClick={() => setShowUsers(!showUsers)}>Users</nobr>
+    
+    //                 {!lisäystila && <button className="nappi" onClick={() => setLisäystila(true)}>Add new</button>}</h1>
+    
+    //                 {!lisäystila && !muokkaustila &&
+    //                 <input placeholder="Search by Last name" value={search} onChange={handleSearchInputChange} />
+    //                 }
+    
+    //                 {lisäystila && <UserAdd setLisäystila={setLisäystila} 
+    //                 setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage}
+    //                 />}
+    
+    //                 {muokkaustila && <UserEdit setMuokkaustila={setMuokkaustila} 
+    //                 setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage}
+    //                 muokattavaUser={muokattavaUser}
+    //                 />}
+    
+    //         {
+    //             !lisäystila && !muokkaustila && showUsers && users && users.map(u =>
+    //               {
+    //                 const lowerCaseName = u.username.toLowerCase()
+    //                 if (lowerCaseName.indexOf(search) > -1) {
+    //                     return(
+    //                 <User key={u.userId} user={u} reloadNow={reloadNow} reload={reload}
+    //                 setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage}
+    //                 editUser={editUser}
+    //                 />
+    //               )
+    //                     }
+    //                   }
+    //             )
+    //         }
+    
+    //     </>
+    //   )
 
 export default UserList
